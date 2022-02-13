9c134dc856cdda49ea28e86c6035a09c633b505a
Mraz Tevin <millatevin@gmail.com>
2022-02-13 20:04:18 +0300

    style object for card

--- src/components/Cards/Card.js
+++ src/components/Cards/Card.js
@@ -1,3 +1,27 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { createUseStyles } from "react-jss";
+
+const useStyles = createUseStyles({
+    add: {
+        background: 'none',
+        border: 'none',
+        cursor: 'pointer',
+    },
+    favorite: {
+        fontSize: 20,
+        position: 'absolute',
+        top: 10,
+        right: 10,
+    },
+    image: {
+        fontSize: 80
+    },
+    wrapper: 'lightgrey solid 1px',
+    margin: 20,
+    padding: 25,
+    position: 'relative',
+    textAlign: 'center',
+    textTransform: 'capitalize',
+    width: 200,
+})
