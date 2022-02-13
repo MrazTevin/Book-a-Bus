72078b02fae7f074ec49e5a07ef50a2080068863
Mraz Tevin <millatevin@gmail.com>
2022-02-13 19:23:44 +0300

    create styles object

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,3 +1,10 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
 
+const useStyles = createUseStyles ( {
+    wrapper: {
+        borderBottom: 'black solid 1px',
+        padding: [15, 10],
+        textAlign: 'right',
+    }
+});
