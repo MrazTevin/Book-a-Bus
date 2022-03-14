87405cc911035ba303e1f3b40af820f5e614ed79
Mraz Tevin <millatevin@gmail.com>
2022-03-14 07:45:11 +0300

    import react router components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import HomePage from './components/Dashboard/HomePage';
-import { BrowserRouter, Rout, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
