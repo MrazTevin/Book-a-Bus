1f55873bd93b7ab9891b92040007edb17f234f2e
Mraz Tevin <millatevin@gmail.com>
2022-03-14 10:20:08 +0300

    add condition and pass settoken to login component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   const[token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
   return (
     <div className="App">
          {/* <Navigation/>  */}
