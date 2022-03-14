358d9d5e5f71ad4afcb876fb73b0cfc463588568
Mraz Tevin <millatevin@gmail.com>
2022-03-14 07:40:15 +0300

    add homepage import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import HomePage from './components/Dashboard/HomePage';
 
 function App() {
   return (
