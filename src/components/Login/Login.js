3ad4291696f2bcbab3e9973232a4790a7a4ae28d
Mraz Tevin <millatevin@gmail.com>
2022-03-15 10:05:01 +0300

    on change function that automatically updates field values

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -11,11 +11,11 @@ const Login = () => {
             <form>
                 <label>
                     <p>Username</p>
-                    <input type="text"/>
+                    <input type="text" onChange={e => setUserName(e.target.value)}/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={e => setPassword(e.target.value)} />
                 </label>
                 <div>
                     <button type="submit">Submit</button>
