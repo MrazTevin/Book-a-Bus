632e230c7d807cd65fe723b033ca5b6442c1b0d6
Mraz Tevin <millatevin@gmail.com>
2022-03-15 09:48:41 +0300

    make few changes

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-import express from 'express';
-// cors is acting as middleware
-import cors from 'cors';
-
-
+// set up express 
 const express = require ('express');
 const cors = require('cors')
 const app = express ();
 
-app.use(cors);
+//listen to specific route
+app.use(cors({origin:true}));
+
+// add login handler
+app.use('/login', (req, res) => {
+    res.send ( {
+        token: 'test334'
+    });
+});
+
+// creating server
+app.listen(8080, () => console.log('API running on http://localhost:8080/login'));
