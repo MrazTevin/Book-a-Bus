c9e19b5a0fc267b8dcc07138c0fe627032bd27fa
Mraz Tevin <millatevin@gmail.com>
2022-03-15 07:38:08 +0300

    instantiate cors

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+// cors is acting as middleware
+import cors from 'cors';
+
+
+const express = require ('express');
+const cors = require('cors')
+const app = express ();
+
+app.use(cors);
